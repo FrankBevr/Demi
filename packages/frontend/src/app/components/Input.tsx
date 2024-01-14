f32d2c337767a17ccaf179eb884c0b2fd182fb94
FrankBevr <frank_dierolf@web.de>
2024-01-15 00:54:20 +0100

    :zap: (frontend) create Input Component

---
+++ packages/frontend/src/app/components/Input.tsx
@@ -0,0 +1,14 @@
+interface InputProps {
+  placeholder?: string;
+}
+
+const Input = ({ placeholder = "Placeholder" }: InputProps) => {
+  return (
+    <input
+      name="input"
+      className="input-default"
+      placeholder={placeholder}
+    />
+  )
+}
+export default Input 
