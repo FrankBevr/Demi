365cfe9b05d0139c312baab799c5ac8233cdc19a
FrankBevr <frank_dierolf@web.de>
2024-01-15 00:57:13 +0100

    :zap: (frontend) clean up, add compoents, align components

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,38 +1,16 @@
 "use client";
-
-import { useEffect } from "react";
-
-import { useInkathon } from "@scio-labs/use-inkathon";
-import { toast } from "react-hot-toast";
-
-import { HomePageTitle } from "@/app/components/home-page-title";
-import { ChainInfo } from "@/components/web3/chain-info";
-import { ConnectButton } from "@/components/web3/connect-button";
-import { GreeterContractInteractions } from "@/components/web3/greeter-contract-interactions";
-
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Logo from "./components/Logo";
 export default function HomePage() {
-  // Display `useInkathon` error messages (optional)
-  const { error } = useInkathon();
-  useEffect(() => {
-    if (!error) return;
-    toast.error(error.message);
-  }, [error]);
 
   return (
     <>
-      <div className="container relative flex grow flex-col items-center justify-center py-10">
-        {/* Title */}
-        <HomePageTitle />
-
-        {/* Connect Wallet Button */}
-        <ConnectButton />
-
-        <div className="mt-12 flex w-full flex-wrap items-start justify-center gap-4">
-          {/* Chain Metadata Information */}
-          <ChainInfo />
-
-          {/* Greeter Read/Write Contract Interactions */}
-          <GreeterContractInteractions />
+      <div className="center-container">
+        <div className="flex-container">
+          <Logo />
+          <Input placeholder="Describe your Data" />
+          <Button buttonText="generate" className="margin-left-auto" />
         </div>
       </div>
     </>
