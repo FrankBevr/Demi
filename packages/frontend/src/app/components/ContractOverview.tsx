af320cf47a1991f2ba45aad6de6f25603529d7e2
FrankBevr <frank_dierolf@web.de>
2024-01-15 16:04:26 +0100

    :zap: (frontend) add ContractOverview Component

--- packages/frontend/src/app/components/ContractOverview.tsx
+++ packages/frontend/src/app/components/ContractOverview.tsx
@@ -1,8 +1,16 @@
+import FunctionComp from "./FunctionComp"
+import VariableComp from "./VariableComp"
+
 const ContractOverview = () => {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="contract-overview-container">
+        <h1>Variables</h1>
+        <VariableComp name="node" description="store AiNode" />
+        <h1>Functions</h1>
+        <FunctionComp buttonText="get_node()" description="A function which return the current AI Node" result="<Address>" />
+      </div>
     </>
   )
 
