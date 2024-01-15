6d607014f0367a8abbe2768021ac3740f697353c
FrankBevr <frank_dierolf@web.de>
2024-01-15 16:03:49 +0100

    :zap: (frontend) add VariableComp

---
+++ packages/frontend/src/app/components/VariableComp.tsx
@@ -0,0 +1,25 @@
+
+interface VariableCompProp {
+  name: string
+  description: string;
+}
+const VariableComp = (
+  {
+    name = "Node",
+    description = "Nodes store all participating ai Nodes",
+  }: VariableCompProp
+) => {
+  return (
+    <>
+      <div>
+        <h2>{name}</h2>
+        <h3>Description</h3>
+        <p>{description}</p>
+        <p>Nodes store all participating ai Nodes</p>
+      </div>
+    </>
+  )
+
+}
+
+export default VariableComp
