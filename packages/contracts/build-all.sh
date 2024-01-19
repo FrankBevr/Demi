84e6f9bc4395263f0540cf120e0f4ff7319baa56
FrankBevr <frank_dierolf@web.de>
2024-01-19 12:59:47 +0100

    :zap: (contracts) update build-all.sh, add argument --debug-on

--- packages/contracts/build-all.sh
+++ packages/contracts/build-all.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # ENVIRONMENT VARIABLES
-CONTRACTS_DIR="${CONTRACTS_DIR:=./src}" # Base contract directory 
+CONTRACTS_DIR="${CONTRACTS_DIR:=./src}" # Base contract directory
 DIR="${DIR:=./deployments}" # Output directory for build files
 
 # Copy command helper (cross-platform)
@@ -11,11 +11,18 @@ CP_CMD=$(command -v cp &> /dev/null && echo "cp" || echo "copy")
 # Determine all contracts under `$CONTRACTS_DIR`
 contracts=($(find $CONTRACTS_DIR -maxdepth 1 -type d -exec test -f {}/Cargo.toml \; -print | xargs -n 1 basename))
 
+# Check if the --debug-on command-line argument was provided
+if [[ "${1:-}" == "--debug-on" ]]; then
+  BUILD_TYPE=""
+else
+  BUILD_TYPE="--release"
+fi
+
 # Build all contracts
 for i in "${contracts[@]}"
 do
   echo -e "\nBuilding '$CONTRACTS_DIR/$i/Cargo.toml'…"
-  cargo contract build --release --quiet --manifest-path $CONTRACTS_DIR/$i/Cargo.toml
+  cargo contract build $BUILD_TYPE --quiet --manifest-path $CONTRACTS_DIR/$i/Cargo.toml
 
   echo "Copying build files to '$DIR/$i/'…"
   mkdir -p $DIR/$i
