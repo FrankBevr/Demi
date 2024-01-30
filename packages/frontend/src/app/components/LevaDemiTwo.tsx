731000ae63d4fbab77542930b0b4dab3bd3b4aac
FrankBevr <frank_dierolf@web.de>
2024-01-30 12:42:33 +0100

    :zap: (frontend) add LevaDemiTwo.tsx

---
+++ packages/frontend/src/app/components/LevaDemiTwo.tsx
@@ -0,0 +1,197 @@
+import React, { useEffect, useState } from "react";
+
+import { ContractIds } from "@/deployments/deployments";
+import { ApiPromise } from "@polkadot/api";
+import { ContractPromise } from "@polkadot/api-contract";
+import { InjectedAccount } from "@polkadot/extension-inject/types";
+import { Signer } from "@polkadot/types/types";
+import {
+  contractQuery,
+  contractTx,
+  decodeOutput,
+  useInkathon,
+  useRegisteredContract,
+} from "@scio-labs/use-inkathon";
+import { button, useControls } from "leva";
+
+interface LevaProps {
+  api?: ApiPromise | undefined;
+  activeAccount?: InjectedAccount | undefined;
+  activeSigner?: Signer | undefined;
+  contract?: ContractPromise | undefined;
+}
+
+const LevaDemiTwo: React.FC<LevaProps> = () => {
+  const { api, activeAccount, activeSigner } = useInkathon();
+  const { contract: contractDemi } = useRegisteredContract(ContractIds.DemiTwo);
+
+  /******/
+  /*DEMI*/
+  /******/
+
+  /*READ*/
+  const [nodesArr, setNodesArr] = useState([""])
+  const [validatorsArr, setValidatorsArr] = useState([""])
+  const [tasksArr, setTasksArr] = useState([""])
+
+  const [, setReadDemiAdv] = useControls("DEMITWO_READ", () => ({
+    nodes: {
+      value: "ALL NODES",
+      options: nodesArr,
+    },
+    validators: {
+      value: "ALL VALIDATORS",
+      options: validatorsArr,
+    },
+    tasks: {
+      value: "ALL TASKS",
+      options: tasksArr,
+    },
+    get_nodes: button(() => getNodes()),
+    get_validators: button(() => getValidators()),
+    get_tasks: button(() => getTasks()),
+  }), [nodesArr, validatorsArr, tasksArr, api, activeAccount, activeSigner]);
+
+  const getNodes = async () => {
+    if (!contractDemi || !api) return;
+    const result = await contractQuery(api, "", contractDemi, "get_nodes");
+    const { output } = decodeOutput(
+      result,
+      contractDemi,
+      "get_nodes",
+    );
+    setNodesArr(output)
+  };
+
+  const getValidators = async () => {
+    if (!contractDemi || !api) return;
+    const result = await contractQuery(api, "", contractDemi, "get_validators");
+    const { output } = decodeOutput(
+      result,
+      contractDemi,
+      "get_validators",
+    );
+    setValidatorsArr(output)
+  };
+
+  const getTasks = async () => {
+    if (!contractDemi || !api) return;
+    const result = await contractQuery(api, "", contractDemi, "get_tasks");
+    const { output } = decodeOutput(
+      result,
+      contractDemi,
+      "get_tasks",
+    );
+    setTasksArr(output)
+  };
+
+
+  useEffect(() => {
+    getNodes();
+    getValidators();
+    getTasks()
+  }, [api, contractDemi]);
+
+  /*******/
+  /*WRITE*/
+  /*******/
+
+  const [newNode, setNewNode] = useState<string>();
+  const [newValidator, setNewValidator] = useState<string>();
+  const [newTask, setNewTask] = useState<string>();
+
+  const [, setNs] = useControls(
+    "DEMITWO_WRITE",
+    () => ({
+      do_init: button(() => {
+        if (!activeAccount || !contractDemi || !activeSigner || !api) {
+          return;
+        }
+        (async () => {
+          await contractTx(
+            api,
+            activeAccount.address,
+            contractDemi,
+            "init",
+            {},
+            [],
+          );
+        })();
+      }),
+      new_nodes: {
+        value: "0x0000",
+        onChange: (c) => {
+          setNewNode(c);
+        },
+      },
+      addNode: button(() => {
+        if (!activeAccount || !contractDemi || !activeSigner || !api) {
+          return;
+        }
+        (async () => {
+          await contractTx(
+            api,
+            activeAccount.address,
+            contractDemi,
+            "add_node",
+            {},
+            [newNode],
+          );
+          setNs({ new_nodes: '' });
+          setNewNode('');
+        })();
+      }),
+      new_validators: {
+        value: "0x0000",
+        onChange: (c) => {
+          setNewNode(c);
+        },
+      },
+      addValidator: button(() => {
+        if (!activeAccount || !contractDemi || !activeSigner || !api) {
+          return;
+        }
+        (async () => {
+          await contractTx(
+            api,
+            activeAccount.address,
+            contractDemi,
+            "add_validator",
+            {},
+            [newNode],
+          );
+          setNs({ new_validators: '' });
+          setNewValidator('');
+        })();
+      }),
+      new_tasks: {
+        value: "",
+        onChange: (c) => {
+          setNewNode(c);
+        },
+      },
+      addTask: button(() => {
+        if (!activeAccount || !contractDemi || !activeSigner || !api) {
+          return;
+        }
+        (async () => {
+          await contractTx(
+            api,
+            activeAccount.address,
+            contractDemi,
+            "add_task",
+            {},
+            [newNode],
+          );
+          setNs({ new_tasks: '' });
+          setNewTask('');
+        })();
+      }),
+    }),
+    [activeAccount, contractDemi, activeSigner, api, newNode, newValidator, newTask],
+  );
+
+  return null;
+};
+
+export default LevaDemiTwo;
