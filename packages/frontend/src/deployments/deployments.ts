e75543d15d941a985f05ffae3582986b9eff386b
FrankBevr <frank_dierolf@web.de>
2024-01-30 12:43:31 +0100

    :zap: (frontend) adjust deployments.ts

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -1,6 +1,14 @@
 import { SubstrateDeployment } from "@scio-labs/use-inkathon";
-
 import { env } from "@/config/environment";
+
+import { address as addressDemione } from '@inkathon/contracts/deployments/demione/development'
+import abiDemione from '@inkathon/contracts/deployments/demione/demione.json'
+
+import { address as addressDemitwo } from '@inkathon/contracts/deployments/demione/development'
+import abiDemitwo from '@inkathon/contracts/deployments/demitwo/demitwo.json'
+
+import { address as addressGreeter } from '@inkathon/contracts/deployments/greeter/development'
+import abiGreeter from '@inkathon/contracts/deployments/greeter/greeter.json'
 
 /**
  * Add or change your custom contract ids here
@@ -8,25 +16,30 @@ import { env } from "@/config/environment";
  */
 export enum ContractIds {
   Greeter = "greeter",
-  Demi = "demi",
+  DemiTwo = "demitwo",
+  DemiOne = "demione"
 }
 
 export const getDeployments = async (): Promise<SubstrateDeployment[]> => {
-  const networks = env.supportedChains;
+  // const networks = env.supportedChains;
   const deployments: SubstrateDeployment[] = [];
+  deployments.push({ "contractId": "greeter", "networkId": "development", "abi": abiGreeter, address: addressGreeter });
+  deployments.push({ "contractId": "demione", "networkId": "development", "abi": abiDemione, address: addressDemione });
+  deployments.push({ "contractId": "demitwo", "networkId": "development", "abi": abiDemitwo, address: addressDemitwo });
 
-  for (const networkId of networks) {
-    for (const contractId of Object.values(ContractIds)) {
-      const abi = await import(
-        `@inkathon/contracts/deployments/${contractId}/${contractId}.json`
-      );
-      const { address } = await import(
-        `@inkathon/contracts/deployments/${contractId}/${networkId}.ts`
-      );
-
-      deployments.push({ contractId, networkId, abi, address });
-    }
-  }
+  // for (const networkId of networks) {
+  //   for (const contractId of Object.values(ContractIds)) {
+  //     if (contractId !== "demione" && contractId !== "demitwo") {
+  //       const abi = await import(
+  //         `@inkathon/contracts/deployments/${contractId}/${contractId}.json`
+  //       );
+  //       const { address } = await import(
+  //         `@inkathon/contracts/deployments/${contractId}/${networkId}.ts`
+  //       );
+  //       deployments.push({ contractId, networkId, abi, address });
+  //     }
+  //   }
+  // }
 
   return deployments;
 };
