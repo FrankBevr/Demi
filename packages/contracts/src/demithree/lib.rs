e1e1d45b4a6330660b0b372d105b081131821622
FrankBevr <frank_dierolf@web.de>
2024-02-01 16:01:14 +0100

    :zap: (contracts) rearange in read/write section

--- packages/contracts/src/demithree/lib.rs
+++ packages/contracts/src/demithree/lib.rs
@@ -19,16 +19,21 @@ mod demithree {
 
     #[ink(storage)]
     pub struct Demithree {
+        //Actors
         owner: AccountId,
-        tasks: Mapping<u32, String>,
-        task_count: u32,
-        validated_tasks: Mapping<u32, ValdiationRating>,
-        validated_tasks_count: u32,
-        is_init: bool,
         registered_nodes: Vec<AccountId>,
         registered_validator: Vec<AccountId>,
         approved_nodes: Vec<AccountId>,
         approved_validator: Vec<AccountId>,
+
+        //Task Managment
+        tasks: Mapping<u32, String>,
+        task_count: u32,
+        validated_tasks: Mapping<u32, ValdiationRating>,
+        validated_tasks_count: u32,
+
+        //Miscellaneous
+        is_init: bool,
     }
 
     impl Demithree {
@@ -47,7 +52,29 @@ mod demithree {
                 approved_validator: Vec::new(),
             }
         }
+        /******/
+        /*READ*/
+        /******/
+        #[ink(message)]
+        pub fn get_init(&mut self) -> bool {
+            self.is_init
+        }
+        #[ink(message)]
+        pub fn get_owner(&self) -> AccountId {
+            self.owner
+        }
+        #[ink(message)]
+        pub fn get_task(&self, index: u32) -> String {
+            self.tasks.get(index).unwrap()
+        }
+        #[ink(message)]
+        pub fn get_validated_rating(&mut self, index: u32) -> ValdiationRating {
+            self.validated_tasks.get(&index).unwrap()
+        }
 
+        /*******/
+        /*WRITE*/
+        /*******/
         #[ink(message)]
         pub fn init(&mut self) {
             if self.owner == AccountId::from([0xFF as u8; 32]) {
@@ -55,68 +82,40 @@ mod demithree {
                 self.is_init = true;
             }
         }
-
-        #[ink(message)]
-        pub fn get_init(&mut self) -> bool {
-            self.is_init
-        }
-
         #[ink(message)]
         pub fn change_owner(&mut self, new_owner: AccountId) {
             if self.owner == self.env().caller() {
                 self.owner = new_owner
             }
         }
-
-        #[ink(message)]
-        pub fn get_owner(&self) -> AccountId {
-            self.owner
-        }
-
         #[ink(message)]
         pub fn add_task(&mut self, new_task: String) {
             self.tasks.insert(self.task_count, &new_task);
             self.task_count += self.task_count + 1;
         }
-
-        #[ink(message)]
-        pub fn get_task(&self, index: u32) -> String {
-            self.tasks.get(index).unwrap()
-        }
-
         #[ink(message)]
         pub fn validate_task(&mut self, index: u32, rating: ValdiationRating) {
             self.validated_tasks.insert(index, &rating);
             self.validated_tasks_count += 1;
         }
-
-        #[ink(message)]
-        pub fn get_validated_rating(&mut self, index: u32) -> ValdiationRating {
-            self.validated_tasks.get(&index).unwrap()
-        }
-
         #[ink(message)]
         pub fn register_node(&mut self) {
             let caller = self.env().caller();
             self.registered_nodes.push(caller)
         }
-
         #[ink(message)]
         pub fn register_validator(&mut self) {
             let caller = self.env().caller();
             self.registered_validator.push(caller)
         }
-
         #[ink(message)]
         pub fn unregister_node(&mut self, node: AccountId) {
             self.registered_nodes.retain(|&x| x != node);
         }
-
         #[ink(message)]
         pub fn unregister_validator(&mut self, validator: AccountId) {
             self.registered_validator.retain(|&x| x != validator);
         }
-
         #[ink(message)]
         pub fn approve_node(&mut self, node: AccountId) {
             if self.env().caller() != self.owner {
@@ -136,7 +135,6 @@ mod demithree {
                 None => {}
             }
         }
-
         #[ink(message)]
         pub fn approved_validator(&mut self, validator: AccountId) {
             if self.env().caller() != self.owner {
