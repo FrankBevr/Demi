e08cde13d5dd47c27531f5f465eb15f3cf0db230
FrankBevr <frank_dierolf@web.de>
2024-02-01 12:44:22 +0100

    :zap: (contracts) add struct, init(), get_init()

--- packages/contracts/src/demithree/lib.rs
+++ packages/contracts/src/demithree/lib.rs
@@ -2,16 +2,43 @@
 
 #[ink::contract]
 mod demithree {
+    use ink::prelude::string::String;
+    use ink::prelude::vec::Vec;
 
     #[ink(storage)]
-    pub struct Demithree {}
+    pub struct Demithree {
+        owner: AccountId,
+        nodes: Vec<AccountId>,
+        validators: Vec<AccountId>,
+        tasks: Vec<String>,
+        is_init: bool,
+    }
 
     impl Demithree {
         #[ink(constructor)]
         pub fn new() -> Self {
-            Self {}
+            Self {
+                owner: AccountId::from([0xFF as u8; 32]),
+                nodes: Vec::new(),
+                validators: Vec::new(),
+                tasks: Vec::new(),
+                is_init: false,
+            }
         }
         #[ink(message)]
         pub fn empty_message(&self) {}
+
+        #[ink(message)]
+        pub fn init(&mut self) {
+            if self.owner == AccountId::from([0xFF as u8; 32]) {
+                self.owner = self.env().caller();
+                self.is_init = true;
+            }
+        }
+
+        #[ink(message)]
+        pub fn get_init(&mut self) -> bool {
+            self.is_init
+        }
     }
 }
