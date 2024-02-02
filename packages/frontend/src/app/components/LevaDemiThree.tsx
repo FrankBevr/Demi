c59a213b911f2714f4a528b7adb976c068fb1167
FrankBevr <frank_dierolf@web.de>
2024-02-02 10:52:16 +0100

    :zap: (frontend) setup read boileplate for LeveDemiThree.tsx

--- packages/frontend/src/app/components/LevaDemiThree.tsx
+++ packages/frontend/src/app/components/LevaDemiThree.tsx
@@ -24,8 +24,26 @@ const LevaDemiThree: React.FC<LevaProps> = () => {
 
   /*READ*/
   const [, setRead] = useControls("DEMITHREE_READ", () => ({
-    some_read_text: "...",
-    some_read_button: button(() => console.log("a button is pressed")),
+    owner: "",
+    registered_nodes: "",
+    registered_validators: "",
+    approved_nodes: "",
+    approved_validators: "",
+    //Task Managment
+    tasks: "",
+    task_count: 0,
+    validated_tasks: "",
+    validated_tasks_count: 0,
+    //Miscellaneous
+    is_init: false,
+    get_init: button(() => get_init()),
+    get_owner: button(() => get_owner()),
+    get_task: button(() => get_task()),
+    get_validated_rating: button(() => get_validated_rating()),
+    get_registered_nodes: button(() => get_registered_nodes()),
+    get_registered_validators: button(() => get_registered_validators()),
+    get_approved_nodes: button(() => get_approved_nodes()),
+    get_approved_validators: button(() => get_approved_validators()),
   }));
 
   /*WRITE*/
@@ -33,6 +51,17 @@ const LevaDemiThree: React.FC<LevaProps> = () => {
     some_write_text: "...",
     some_write_button: button(() => console.log("a button is pressed")),
   }));
+
+  /*READ Functions*/
+  const get_init = async () => { }
+  const get_owner = async () => { }
+  const get_task = async () => { }
+  const get_validated_rating = async () => { }
+  const get_registered_nodes = async () => { }
+  const get_registered_validators = async () => { }
+  const get_approved_nodes = async () => { }
+  const get_approved_validators = async () => { }
+
 
   return null;
 };
