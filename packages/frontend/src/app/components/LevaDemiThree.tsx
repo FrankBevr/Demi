406975741d4b86696c4b143c0cd20af18f76059c
FrankBevr <frank_dierolf@web.de>
2024-02-03 08:42:22 +0100

    :zap: (frontend) rearange input formats, add get_owner(), init()

--- packages/frontend/src/app/components/LevaDemiThree.tsx
+++ packages/frontend/src/app/components/LevaDemiThree.tsx
@@ -5,7 +5,13 @@ import { ApiPromise } from "@polkadot/api";
 import { ContractPromise } from "@polkadot/api-contract";
 import { InjectedAccount } from "@polkadot/extension-inject/types";
 import { Signer } from "@polkadot/types/types";
-import { useInkathon, useRegisteredContract } from "@scio-labs/use-inkathon";
+import {
+  contractQuery,
+  contractTx,
+  decodeOutput,
+  useInkathon,
+  useRegisteredContract,
+} from "@scio-labs/use-inkathon";
 import { button, useControls } from "leva";
 
 interface LevaProps {
@@ -16,7 +22,7 @@ interface LevaProps {
 }
 const LevaDemiThree: React.FC<LevaProps> = () => {
   const { api, activeAccount, activeSigner } = useInkathon();
-  const { contract: contractGreeter } = useRegisteredContract(
+  const { contract: contractDemiThree } = useRegisteredContract(
     ContractIds.DemiThree,
   );
 
@@ -43,78 +49,129 @@ const LevaDemiThree: React.FC<LevaProps> = () => {
   /*Miscellaneous*/
   const [isInit, setIsInit] = useState(false);
 
-  const [, setRead] = useControls("DEMITHREE_READ", () => ({
-    //Actors
-    owner: {
-      value: owner,
-      disabled: true,
-    },
-    registered_nodes: {
-      value: "ALL REGISTERED NODES",
-      options: registered_nodesArr,
-    },
-    registered_validators: {
-      value: "ALL REGISTERED VALIDATORS",
-      options: registered_validatorsArr,
-    },
-    approved_nodes: {
-      value: "ALL APPROVED NODES",
-      options: approved_nodesArr,
-    },
-    approved_validators: {
-      value: "ALL APPROVED VALIDATORS",
-      options: approved_validatorsArr,
-    },
+  const [, setRead] = useControls(
+    "DEMITHREE_READ",
+    () => ({
+      //Actors
+      owner: {
+        value: owner,
+        disabled: true,
+      },
+      registered_nodes: {
+        value: "ALL REGISTERED NODES",
+        options: registered_nodesArr,
+      },
+      registered_validators: {
+        value: "ALL REGISTERED VALIDATORS",
+        options: registered_validatorsArr,
+      },
+      approved_nodes: {
+        value: "ALL APPROVED NODES",
+        options: approved_nodesArr,
+      },
+      approved_validators: {
+        value: "ALL APPROVED VALIDATORS",
+        options: approved_validatorsArr,
+      },
 
-    //Task Managment
-    tasks: {
-      value: "ALL TASKS",
-      options: tasksArr,
-    },
-    task_count: {
-      value: task_count,
-      disabled: true,
-    },
-    validated_tasks: {
-      value: "ALL VALIDATED TASKS",
-      options: validated_tasksArr,
-    },
-    validated_tasks_count: {
-      value: validated_task_count,
-      disabled: true,
-    },
+      //Task Managment
+      tasks: {
+        value: "ALL TASKS",
+        options: tasksArr,
+      },
+      task_count: {
+        value: task_count,
+        disabled: true,
+      },
+      validated_tasks: {
+        value: "ALL VALIDATED TASKS",
+        options: validated_tasksArr,
+      },
+      validated_tasks_count: {
+        value: validated_task_count,
+        disabled: true,
+      },
 
-    //Miscellaneous
-    is_init: {
-      value: isInit,
-      disabled: true,
-    },
+      //Miscellaneous
+      is_init: {
+        value: isInit,
+        disabled: true,
+      },
 
-    //Actors
-    get_owner: button(() => get_owner()),
-    get_registered_nodes: button(() => get_registered_nodes()),
-    get_registered_validators: button(() => get_registered_validators()),
-    get_approved_nodes: button(() => get_approved_nodes()),
-    get_approved_validators: button(() => get_approved_validators()),
+      //Actors
+      get_owner: button(() => get_owner()),
+      get_registered_nodes: button(() => get_registered_nodes()),
+      get_registered_validators: button(() => get_registered_validators()),
+      get_approved_nodes: button(() => get_approved_nodes()),
+      get_approved_validators: button(() => get_approved_validators()),
 
-    //Task Managment
-    get_tasks: button(() => get_tasks()),
-    get_task_count: button(() => get_task_count()),
-    get_validated_tasks: button(() => get_validated_tasks()),
+      //Task Managment
+      get_tasks: button(() => get_tasks()),
+      get_task_count: button(() => get_task_count()),
+      get_validated_tasks: button(() => get_validated_tasks()),
 
-    //Miscellaneous
-    get_init: button(() => get_init()),
-  }));
+      //Miscellaneous
+      get_init: button(() => get_init()),
+    }),
+    [
+      activeAccount,
+      contractDemiThree,
+      activeSigner,
+      api,
+      owner,
+      registered_nodesArr,
+      registered_validatorsArr,
+      approved_nodesArr,
+      approved_validatorsArr,
+      tasksArr,
+      task_count,
+      validated_tasksArr,
+      validated_task_count,
+      isInit,
+    ],
+  );
 
   /*WRITE*/
-  const [, setWrite] = useControls("DEMITHREE_WRITE", () => ({
-    some_write_text: "...",
-    some_write_button: button(() => console.log("a button is pressed")),
-  }));
+  const [, setWrite] = useControls(
+    "DEMITHREE_WRITE",
+    () => ({
+      init: button(() => init()),
+    }),
+    [
+      activeAccount,
+      contractDemiThree,
+      activeSigner,
+      api,
+      owner,
+      registered_nodesArr,
+      registered_validatorsArr,
+      approved_nodesArr,
+      approved_validatorsArr,
+      tasksArr,
+      task_count,
+      validated_tasksArr,
+      validated_task_count,
+      isInit,
+    ],
+  );
 
+  /****************/
   /*READ Functions*/
+  /****************/
   //Actors
-  const get_owner = async () => {};
+  const get_owner = async () => {
+    console.log("get_owner was called");
+    if (!contractDemiThree || !api) return;
+    const result = await contractQuery(api, "", contractDemiThree, "get_owner");
+    const { output, isError, decodedOutput } = decodeOutput(
+      result,
+      contractDemiThree,
+      "get_owner",
+    );
+    if (isError) throw new Error(decodedOutput);
+    setRead({ owner: output });
+    set_owner(output);
+  };
   const get_registered_nodes = async () => {};
   const get_registered_validators = async () => {};
   const get_approved_nodes = async () => {};
@@ -126,7 +183,33 @@ const LevaDemiThree: React.FC<LevaProps> = () => {
   const get_validated_tasks = async () => {};
 
   //Miscellaneous
-  const get_init = async () => {};
+  const get_init = async () => {
+    console.log("get_init was called");
+    if (!contractDemiThree || !api) return;
+    const result = await contractQuery(api, "", contractDemiThree, "get_init");
+    const { output } = decodeOutput(result, contractDemiThree, "get_init");
+    setRead({ is_init: output });
+    setIsInit(output);
+  };
+
+  /*****************/
+  /*WRITE Functions*/
+  /*****************/
+  const init = async () => {
+    console.log(contractDemiThree);
+    if (!activeAccount || !contractDemiThree || !activeSigner || !api) {
+      return;
+    }
+    await contractTx(
+      api,
+      activeAccount.address,
+      contractDemiThree,
+      "init",
+      {},
+      [],
+    );
+    await get_init();
+  };
 
   return null;
 };
